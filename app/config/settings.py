8eb66ee8148f959b9d6566d5547c271c2bfa1a41
alexandr <dmitrymaslov2016@mai.ru>
2023-12-13 23:40:17 +0300

    add docs

--- app/config/settings.py
+++ app/config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework.authtoken',
     'rest_framework_simplejwt.token_blacklist',
+    'drf_yasg',
 
     'users',
     'complation',
