be3266bfbcda25cec8f135616740f017476f1eb9
alexandr <dmitrymaslov2016@mai.ru>
2023-12-20 19:47:09 +0300

    create types_website.py

--- app/bookmark/models.py
+++ app/bookmark/models.py
@@ -1,25 +1,12 @@
 from django.db import models
+from .types_website import TypesWebsite
 
 
 class Bookmark(models.Model):
-    WEBSITE = 'website'
-    BOOK = 'book'
-    ARTICLE = 'article'
-    MUSIC = 'music'
-    VIDEO = 'video'
-
-    TYPES_LINKS = (
-        (WEBSITE, 'website'),
-        (BOOK, 'book'),
-        (ARTICLE, 'article'),
-        (MUSIC, 'music'),
-        (VIDEO, 'video')
-    )
-
     title = models.CharField(max_length=255, verbose_name='заголовок')
     description = models.TextField(verbose_name='краткое описание')
     url = models.URLField(verbose_name='ссылка')
-    type = models.CharField(choices=TYPES_LINKS, default='website', verbose_name='тип ссылки')
+    type = models.CharField(choices=TypesWebsite.TYPES_LINKS, default='website', verbose_name='тип ссылки')
     image = models.ImageField(max_length=255, upload_to='bookmarks', verbose_name='картинка', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='дата обновления')
---
+++ app/bookmark/types_website.py
@@ -0,0 +1,14 @@
+class TypesWebsite:
+    WEBSITE = 'website'
+    BOOK = 'book'
+    ARTICLE = 'article'
+    MUSIC = 'music'
+    VIDEO = 'video'
+
+    TYPES_LINKS = (
+        (WEBSITE, 'website'),
+        (BOOK, 'book'),
+        (ARTICLE, 'article'),
+        (MUSIC, 'music'),
+        (VIDEO, 'video')
+    )
